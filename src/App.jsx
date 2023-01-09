6ab7e329d6328648d411552e83c70dac7bf41882
Anderson-Rodrigo-Dev <andersonrodrigodev@gmail.com>
2023-01-09 17:37:37 -0300

    projeto MoviesLib

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <Navbar />
+      <Outlet />
+    </div>
+  );
+}
+
+export default App;
